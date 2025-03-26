447be0aa3a0d8b66460868d9bb207e3f1f0c51a6
Mukul Joshi <mjoshi1819@gmail.com>
2025-03-27 01:30:01 +0530

    Update page title and enhance footer styling for better layout

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { FaRegCopyright } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <footer className="flex fixed left-0 bottom-0 right-0 h-[80px] justify-end items-center font-[arial]">
+    <footer className="mt-auto flex h-[80px] justify-end items-center font-[arial] bg-transparent">
       <FaRegCopyright />{" "}
       <span className="pl-2 pr-24"> All rights reserved to Mukul Joshi</span>
     </footer>
