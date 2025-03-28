b5dc586b3e0973c9564faf851dd705491d004459
Mukul Joshi <mjoshi1819@gmail.com>
2025-03-29 02:51:38 +0530

    Update CORS configuration to use frontend URI from environment variable and add .env to .gitignore

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: `${process.env.FRONTEND_URI}` }));
 
 // Routes import
 import { bucketRoutes } from "./routes/bucket.routes.js";
