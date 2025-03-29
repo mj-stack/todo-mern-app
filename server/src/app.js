35dcd9a56ca91b87ac6d3e5868849168faa1bb0d
Mukul Joshi <mjoshi1819@gmail.com>
2025-03-29 18:13:04 +0530

    Fixed CORS configuration to allow multiple origins and improve error handling

--- server/src/app.js
+++ server/src/app.js
@@ -5,9 +5,26 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config({ path: "./.env" });
 
+const allowedOrigins = [
+  process.env.FRONTEND_URI,
+  "https://todo-mern-app-nu.vercel.app",
+];
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors({ origin: process.env.FRONTEND_URI }));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 
 // Routes import
 import { bucketRoutes } from "./routes/bucket.routes.js";
